d86d2153b3da15a97271d37c75d954a3f9d1cffa
millieXie <167055589+millieXie@users.noreply.github.com>
2024-10-11 00:52:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import numpy as np
 import os
 import torch
 import time
-import torchvision.transforms as transforms
+from PIL import Image, ImageOps
 from models.vmamba_Fusion_efficross import VSSM_Fusion as net
-from PIL import Image
+# from loss import Fusionloss
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 model = net(in_channel=1)
-model_path = "./madelpath.pth"
+model_path = "./model_last/my_cross/model.pth" # put the path of model.pth
 use_gpu = torch.cuda.is_available()
 
 if use_gpu:
@@ -23,76 +23,84 @@ else:
     model.load_state_dict(state_dict)
 
 
-# def imresize(image, size, interp='bilinear'):
-#     interp_methods = {
-#         'nearest': cv2.INTER_NEAREST,
-#         'bilinear': cv2.INTER_LINEAR,
-#         'bicubic': cv2.INTER_CUBIC,
-#         'lanczos': cv2.INTER_LANCZOS4
-#     }
-#     resized_image = cv2.resize(image, (size[1], size[0]), interpolation=interp_methods.get(interp, cv2.INTER_LINEAR))
-#     return resized_image
+def imresize(arr, size, interp='bilinear', mode=None):
+    numpydata = np.asarray(arr)
+    im = Image.fromarray(numpydata, mode=mode)
+    ts = type(size)
+    if np.issubdtype(ts, np.signedinteger):
+        percent = size / 100.0
+        size = tuple((np.array(im.size) * percent).astype(int))
+    elif np.issubdtype(type(size), np.floating):
+        size = tuple((np.array(im.size) * size).astype(int))
+    else:
+        size = (size[1], size[0])
+    func = {'nearest': 0, 'lanczos': 1, 'bilinear': 2, 'bicubic': 3, 'cubic': 3}
+    imnew = im.resize(size, resample=func[interp])
+    return np.array(imnew)
+
+
+def resize(image1, image2, crop_size_img, crop_size_label):
+    image1 = imresize(image1, crop_size_img, interp='bicubic')
+    image2 = imresize(image2, crop_size_label, interp='bicubic')
+    return image1, image2
+
+
+def get_image_files(input_folder):
+    valid_extensions = (".bmp", ".tif", ".jpg", ".jpeg", ".png")
+    return sorted([f for f in os.listdir(input_folder) if f.lower().endswith(valid_extensions)])
+
 
 def fusion(input_folder_ir, input_folder_vis, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     tic = time.time()
+    # criteria_fusion = Fusionloss()
 
-
-    ir_images = sorted([f for f in os.listdir(input_folder_ir) if f.endswith('.png')])
-    vis_images = sorted([f for f in os.listdir(input_folder_vis) if f.endswith('.png')])
+    ir_images = get_image_files(input_folder_ir)
+    vis_images = get_image_files(input_folder_vis)
 
     for ir_image, vis_image in zip(ir_images, vis_images):
         path1 = os.path.join(input_folder_ir, ir_image)
         path2 = os.path.join(input_folder_vis, vis_image)
 
-        # 读取灰度图像
-        img1 = cv2.imread(path1, 0)
-        img2 = cv2.imread(path2, 0)
+        img1 = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.imread(path2, cv2.IMREAD_GRAYSCALE)
 
-        # 归一化图像
-        img1 = np.asarray(Image.fromarray(img1), dtype=np.float32) / 255.0
-        img2 = np.asarray(Image.fromarray(img2), dtype=np.float32) / 255.0
+        img1, img2 = resize(img1, img2, [256, 256], [256, 256]) #
 
-        # 扩展维度
+        img1 = np.asarray(img1, dtype=np.float32) / 255.0
+        img2 = np.asarray(img2, dtype=np.float32) / 255.0
+
         img1 = np.expand_dims(img1, axis=0)
         img2 = np.expand_dims(img2, axis=0)
 
-        # 转换为张量
         img1_tensor = torch.from_numpy(img1).unsqueeze(0).to(device)
         img2_tensor = torch.from_numpy(img2).unsqueeze(0).to(device)
 
         model.eval()
         with torch.no_grad():
             out = model(img1_tensor, img2_tensor)
-            out = torch.clamp(out, 0, 1) 
+            out = torch.clamp(out, 0, 1)  
             
-            # 将张量转换为 NumPy 数组
             out_np = out.cpu().numpy()
             
-            # 归一化输出
             out_np = (out_np - np.min(out_np)) / (np.max(out_np) - np.min(out_np))
 
-        # 处理和保存结果
         d = np.squeeze(out_np)
         result = (d * 255).astype(np.uint8)
 
-        output_path = os.path.join(output_folder, ir_image)
+        output_filename = os.path.splitext(ir_image)[0] + os.path.splitext(ir_image)[1]
+        output_path = os.path.join(output_folder, output_filename)
         cv2.imwrite(output_path, result)
 
     toc = time.time()
     print('Processing time: {}'.format(toc - tic))
 
 
-
-
 if __name__ == '__main__':
-  
-    input_folder_1 = '/path1'
-    input_folder_2 = '/path2'
-    output_folder = './results'
+    input_folder_1 = '/root/autodl-tmp/FusionMamba_last/images/ir_1'
+    input_folder_2 = '/root/autodl-tmp/FusionMamba_last/images/vis_1'
+    output_folder = './outputs'
 
     fusion(input_folder_2, input_folder_1, output_folder)
-
-
